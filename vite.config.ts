ef080bfb68e4c9de957282d6c538235e7bcf2325
Kevin Radino (Aleacsys) <kevinr.be@gmail.com>
2024-07-10 10:03:41 +0200

    dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const port = Number(process.env.PORT) || 3000;
+
 export default defineConfig({
     plugins: [vue()],
     build: {
-        outDir: '../dist',
+        outDir: './dist',
+    },
+    server: {
+        host: true,
+        port: port,
+        watch: {
+            usePolling: true,
+        },
     },
 });
